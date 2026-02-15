cc19b309fd01a31a038567042e202c40cb11433e
𝖸𝖺𝗁𝗂𝗄𝗈 !! <lazinessruler@gmail.com>
2026-02-15 19:22:23 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,6 +2,6 @@ from plugins.start import router as start_router
 from plugins.settings import router as settings_router
 from plugins.video import router as video_router
 from plugins.admin import router as admin_router
-from plugins.uptime import router as uptime_router  
+from plugins.uptime import router as uptime_router
 
 __all__ = ["start_router", "settings_router", "video_router", "admin_router", "uptime_router"]
